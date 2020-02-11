1ceab42b9ba61977ae40ee6cacf2bb1bcdbae55a
AnantaSrikar <srikarananta01@gmail.com>
2020-02-12 02:53:26 +0530

    Added logout feature

--- cogs/ownerCog.py
+++ cogs/ownerCog.py
@@ -1,5 +1,6 @@
 import discord 
 from discord.ext import commands
+from asyncio import sleep
 
 class ownerCog(commands.Cog):
     def __init__(self, bot):
@@ -13,7 +14,11 @@ class ownerCog(commands.Cog):
     @commands.command('logout')
     @commands.is_owner()
     async def logout(self, ctx):
-        await ctx.channel.send('Owner detected, will logout once feature is added')
+        await ctx.channel.send("Initiating logout")
+        await self.bot.change_presence(status = discord.Status.offline)
+        await sleep(1)
+        await self.bot.logout()
+        print("Logged out successfully")
 
 def setup(bot):
     bot.add_cog(ownerCog(bot))
