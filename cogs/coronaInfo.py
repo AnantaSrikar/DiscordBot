e32afbce48c6f45ea7713ff900e15f8c33381b4f
Ananta Srikar <srikarananta01@gmail.com>
2020-03-10 01:17:14 +0500

    Alpha stage of covid

--- cogs/coronaInfo.py
+++ cogs/coronaInfo.py
@@ -1,19 +1,33 @@
 import requests
 import PyPDF2
-url = 'https://www.who.int/docs/default-source/coronaviruse/situation-reports/20200304-sitrep-44-covid-19.pdf'
-usl = 'https://www.who.int/docs/default-source/coronaviruse/situation-reports/20200303-sitrep-43-covid-19.pdf'
+url = 'https://www.who.int/docs/default-source/coronaviruse/situation-reports/20200309-sitrep-49-covid-19.pdf'
 
-try:
-    myfile = requests.get(url)
-    open('testfile.pdf', 'wb').write(myfile.content)
-    pdfFileObj = open('testfile.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-    pdfFileObj.close() 
+def getInfo():
 
-    print(pdfReader.numPages) 
+    try:
+        myfile = requests.get(url)
+        open('testfile.pdf', 'wb').write(myfile.content)
+        pdfFileObj = open('testfile.pdf', 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+        pageObj = pdfReader.getPage(4)
+        data = pageObj.extractText()
+        simplifiedData = data.split('\n')
 
-except PyPDF2.utils.PdfReadError:
-    print('Todays not yet released')
+        emptySpace = ' '
+        for emptySpace in simplifiedData:
+            try:
+                simplifiedData.remove(emptySpace)
+            except ValueError:
+                pass
+        indiaIndex = simplifiedData.index('India')
+        textdata = 'Total Confirmed Cases = {}\nTotal Confirmed new cases = {}\nTotal Deaths = {}\nTotal new Deaths = {}\nTransmission Classification : {}\nDays since last report case : {}'.format(simplifiedData[indiaIndex + 1], simplifiedData[indiaIndex + 2], simplifiedData[indiaIndex + 3], simplifiedData[indiaIndex + 4], simplifiedData[indiaIndex + 5], simplifiedData[indiaIndex + 6])
+        #dataFile = open('data.txt', 'r')
+        pdfFileObj.close()
+        return textdata
+        #print(pdfReader.numPages)
+
+    except PyPDF2.utils.PdfReadError:
+        return ('Todays not yet released')
 
   
 # printing number of pages in pdf file 
--- cogs/covCog.py
+++ cogs/covCog.py
@@ -1,5 +1,6 @@
 import discord 
 from discord.ext import commands
+import cogs.coronaInfo
 
 class covCog(commands.Cog):
     def __init__(self, bot):
@@ -7,7 +8,8 @@ class covCog(commands.Cog):
 
     @commands.command('covid')
     async def tester(self, ctx):
-        await ctx.channel.send('Stay tuned {}! COVID-19 update feature coming up soon.'.format(ctx.message.author.mention))
+        #await ctx.channel.send('Stay tuned {}! COVID-19 update feature coming up soon.'.format(ctx.message.author.mention))
+        await ctx.channel.send(coronaInfo.getInfo())
 
 def setup(bot):
     bot.add_cog(covCog(bot))
